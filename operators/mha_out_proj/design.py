e31fefb6795f5fa96bfbbb2f982dfc8b0bbc1977
kurtis-b <22698418+kurtis-b@users.noreply.github.com>
2026-02-23 09:49:03 -0700

    Ffn an new 1 (#8)
    * Found way to use CT DMAs to tile microkernel dims for softmax to PV--this removes need for MT connections and allows for those MT DMAs to be used for something else
    
    * Also now using CT DMAs to tile from microkernel dims to row maj layout for QK to softmax--removes need to use MT DMAs for those as well; NOTE: Didn't test debug mode 1 or 2 in this and prev commit, assumed that they still work properly
    
    * Initial commit--copied old design file
    
    * Depracate op.py since I'll be making a bunch of changes, which will likely include this file
    
    * Initial commit--copied from deprecated file
    
    * Rename folder since I will remove the first AN stage from the pipeline in this design--that stage should be added incrementally to the MHA pipeline instead since it requires a neighboring connection for input
    
    * UNTESTED: Removing ln1 from the design since ln needs neighboring connection
    
    * Removed ln1
    
    * Debug mode 0 failes, 1 passes; removed ln1 use in op, reference, and test files
    
    * Simple tests pass (didn't test all of them, just the ones up to bert workload); bert workload tests pass for one configuration, need to debug other configuration, though it runs a little slower
    
    * Re-enabled failing test, and switched debug mode to disabled; moving onto switching layer norm method instead of fixing test to save time
    
    * UNTESTED: changing how layer norm operation is executed
    
    * Initial change to layer norm execution--some basic tests already pass but runlist execution fails when running stage 1 (down proj stage); need to debug
    
    * Simple tests run now, but need to fix sum/sumsq calculations since it seems the tiling logic for calculations is wrong
    
    * Got most basic tests to pass--looks like can't buffer tiles from LN to MTs with the same MT that down proj uses to buffer tiles (runs out of BDs); need to change how the design executes again to address this; will just stream the tiles from down proj core twice for the two passes that LN core does
    
    * Update names since since I switched it for fused ffn with add and norm
    
    * Changed LN execution to fix BD issue with design due to LN core streaming to MT; changes broke something that caused all functional basic tests to fail; basic tests mostly pass and the design with scaling down proj depth now compiles but still functionally incorrect
    
    * FIXED tests failing with scaling down proj depth parameter
    
    * Fixed mapping bug with tests scaling a tile parallelism
    
    * Simple tests all now pass, the non-simple BERT workload tests run but fail verification
    
    * ALL TESTS NOW PASS
    
    * Disable running simple tests
    
    * Simple optimization to LN kernels by manually unrolling the colA loop by half
    
    * Added more tests to see how design scales, and wrote TODOs to address later

--- operators/mha_out_proj/design.py
+++ operators/mha_out_proj/design.py
@@ -26,6 +26,8 @@ from aie.iron.device import NPU1Col1, NPU2, Tile
 from aie.iron.controlflow import range_
 from aie.helpers.taplib import TensorTiler2D, TensorAccessSequence, TensorAccessPattern
 from aie.helpers.dialects.ext.scf import if_, else_
+import aie.dialects.index as index
+from aie.dialects.aiex import *
 
 base_dir = Path(__file__).parent
 
@@ -112,7 +114,6 @@ def fused_mha(
     embed_sz = heads * d
 
     of_depth = 2
-    vectorized = True
     enable_tracing = True if trace_size > 0 else False
     dtype_str = "bf16"
     dev = "npu2"
@@ -140,7 +141,6 @@ def fused_mha(
     )
     logging.info(f"Data type: {dtype_str}")
     logging.info(f"Microkernel MAC dimensions: r={r}, s={s}, t={t}")
-    logging.info(f"Vectorized: {vectorized}")
     logging.info(f"Enable tracing: {enable_tracing}")
 
     assert heads > 0, "Number of heads must be greater than 0"
@@ -185,7 +185,6 @@ def fused_mha(
     o_ty = np.ndarray[(seq_tile, emb_tile), np.dtype[dtype]]
 
     # AIE kernel declarations
-    func_type = "" if vectorized else "_scalar"
     bin_name = kernel_archive
 
     zero_kernel = Kernel(f"zero_{dtype_str}", bin_name, [qk_ty])
@@ -211,7 +210,7 @@ def fused_mha(
     )
 
     matmul_QK = Kernel(
-        f"matmul_bf16_bf16_wrapper{func_type}",
+        f"matmul_bf16_bf16_wrapper",
         bin_name,
         [q_ty, k_ty, qk_ty, np.ndarray[(2,), np.dtype[np.int32]]],
     )
@@ -258,9 +257,7 @@ def fused_mha(
     )
 
     # AIE-array data movement with object fifos
-    q_dims = None
-    if vectorized:
-        q_dims = [(seq_tile // r, r * d), (d // s, s), (r, d), (s, 1)]
+    q_dims = [(seq_tile // r, r * d), (d // s, s), (r, d), (s, 1)]
 
     inQ = ObjectFifo(
         np.ndarray[(seq_tile, d * parallel_heads), np.dtype[dtype]],
@@ -280,9 +277,7 @@ def fused_mha(
     # I think the Sequential Placer will fail if we do a split/join with more than 6 I/Os cuz it tries to place them all on the same tile.
 
     # K is stored in column-major order
-    k_dims = None
-    if vectorized:
-        k_dims = [(seq_tile // t, t * d), (d // s, s), (t, d), (s, 1)]
+    k_dims = [(seq_tile // t, t * d), (d // s, s), (t, d), (s, 1)]
     inK = ObjectFifo(
         np.ndarray[(seq_tile, d * parallel_heads), np.dtype[dtype]],
         name="inK",
@@ -297,14 +292,12 @@ def fused_mha(
         placement=Tile(col=1, row=1),
     )  # Split between N parallel blocks of heads
 
-    v_dims = None
-    if vectorized:
-        v_dims = [
-            (seq_tile // s, s * seq_tile),
-            (d // t, t),
-            (s, seq_tile),
-            (t, 1),
-        ]
+    v_dims = [
+        (seq_tile // s, s * seq_tile),
+        (d // t, t),
+        (s, seq_tile),
+        (t, 1),
+    ]
 
     inV = ObjectFifo(
         np.ndarray[(seq_tile, d * parallel_heads), np.dtype[dtype]],
@@ -320,38 +313,38 @@ def fused_mha(
         placement=Tile(col=2, row=1),
     )  # Split between N parallel blocks of heads
 
-    a_dims = None
-    if vectorized:
-        a_dims = [(seq_tile // r, r * seq_tile), (r, t), (seq_tile // t, r * t), (t, 1)]
     memA = []
-    outA = []
+    a_dims = [(seq_tile // r, r * seq_tile), (r, t), (seq_tile // t, r * t), (t, 1)]
+    # First send microkernel tiles across the sequence dimension of the output,
+    # then place those microkernel tiles in the correct locations with another DMA
+    a_dims_out = [(seq_tile // s, s), (seq_tile, r * s), (s, 1)]
+    a_dims_in = [(seq_tile // s, s), (seq_tile // s, seq_tile), (s, 1)]
     for i in range(parallel_heads):
-        memA.append(ObjectFifo(qk_ty, depth=of_depth, name=f"memA{i}"))
-        outA.append(
-            memA[i]
-            .cons()
-            .forward(
-                name=f"outA{i}",
-                dims_to_stream=a_dims,
+        memA.append(
+            ObjectFifo(
+                qk_ty,
                 depth=of_depth,
-                placement=Tile(col=4, row=1),
+                name=f"memA{i}",
+                dims_to_stream=a_dims_out,
+                dims_from_stream_per_cons=a_dims_in,
             )
-        )  # Local to 1 parallel block of sequences
+        )  # Local to 1 parallel block of heads
 
     memP = []
-    outP = []
+    # First send microkernel tiles across the sequence dimension of the output,
+    # then place those microkernel tiles in the correct locations with another DMA
+    p_dims_out = [(seq_tile // s, s), (seq_tile, seq_tile), (s, 1)]
+    p_dims_in = [(seq_tile // s, r * s), (seq_tile // r, seq_tile * r), (r * s, 1)]
     for i in range(parallel_heads):
-        memP.append(ObjectFifo(qk_ty, depth=of_depth, name=f"memP{i}"))
-        outP.append(
-            memP[i]
-            .cons()
-            .forward(
-                name=f"outP{i}",
-                dims_to_stream=q_dims,
+        memP.append(
+            ObjectFifo(
+                qk_ty,
                 depth=of_depth,
-                placement=Tile(col=5, row=1),
+                name=f"memP{i}",
+                dims_to_stream=p_dims_out,
+                dims_from_stream_per_cons=p_dims_in,
             )
-        )  # Local to 1 parallel block of sequences
+        )  # Local to 1 parallel block of heads
 
     # Scale buffer for partial softmax
     scaleOF = []
@@ -361,14 +354,12 @@ def fused_mha(
         )  # Local to 1 parallel block of sequences
 
     # Output projection weights
-    ow_dims = None
-    if vectorized:
-        ow_dims = [
-            (d // s, s * emb_tile),
-            (emb_tile // t, t),
-            (s, emb_tile),
-            (t, 1),
-        ]
+    ow_dims = [
+        (d // s, s * emb_tile),
+        (emb_tile // t, t),
+        (s, emb_tile),
+        (t, 1),
+    ]
 
     inOW = ObjectFifo(
         np.ndarray[(d * parallel_heads, emb_tile), np.dtype[dtype]],
@@ -409,9 +400,7 @@ def fused_mha(
             ObjectFifo(o_ty, depth=of_depth, name=f"outOPart{i}")
         )  # Local to 1 parallel block of heads
 
-    o_dims = None
-    if vectorized:
-        o_dims = [(seq_tile // r, r * emb_tile), (r, t), (emb_tile // t, r * t), (t, 1)]
+    o_dims = [(seq_tile // r, r * emb_tile), (r, t), (emb_tile // t, r * t), (t, 1)]
     memO = ObjectFifo(
         np.ndarray[(seq_tile, emb_tile), np.dtype[dtype]],
         name="memO",
@@ -733,7 +722,7 @@ def fused_mha(
             Worker(
                 softmax,
                 fn_args=[
-                    outA[i].cons(),
+                    memA[i].cons(),
                     memP[i].prod(),
                     scaleOF[i].prod(),
                     partial_softmax_kernel,
@@ -756,7 +745,7 @@ def fused_mha(
             Worker(
                 batched_matmul_pv,
                 fn_args=[
-                    outP[i].cons(),
+                    memP[i].cons(),
                     memV[i].cons(),
                     scaleOF[i].cons(),
                     outOProj[i].prod(),
