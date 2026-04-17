ba8de695e080b4c1b6ea53f9fa1f26cced6ac6b2
Curt Bansil <curtjohnb@gmail.com>
2026-04-16 19:35:30 -0700

    Add standalone dynamic GEMM operator

---
+++ iron/operators/dynamic_gemm/reference.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from iron.operators.gemm.reference import generate_golden_reference
+
+__all__ = ["generate_golden_reference"]
